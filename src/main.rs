f7c9acc901bab22b047421d0636fc439c8f82cd6
Phạm Quốc Hưng <165866405+LeviathanST@users.noreply.github.com>
2024-08-09 15:32:06 +0000

    init env file and handle error file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod api;
 mod config;
+mod error;
 
 use std::net::SocketAddr;
 
+use dotenv::dotenv;
 use tokio::net::TcpListener;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -12,6 +14,8 @@ async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer().pretty())
         .init();
+
+    dotenv().ok();
 
     let listener = TcpListener::bind(SocketAddr::new([0, 0, 0, 0].into(), *config::SERVER_PORT))
         .await

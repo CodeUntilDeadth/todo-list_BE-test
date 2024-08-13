8d32d32b4782afdeb64491a06fde1f3a9c987a7b
Phạm Quốc Hưng <165866405+LeviathanST@users.noreply.github.com>
2024-08-13 09:14:14 +0000

    refactor: move to folder auth

--- src/api/controller/auth/oauth2_gg.rs
+++ src/api/controller/auth/oauth2_gg.rs
@@ -1,18 +1,16 @@
-use anyhow::Result;
 use axum::{
-    extract::Query,
-    http::StatusCode,
+    extract::{Query, State},
     response::{IntoResponse, Redirect},
     Json,
 };
-use oauth2::{
-    basic::BasicClient, reqwest::async_http_client, AuthUrl, AuthorizationCode, ClientId,
-    ClientSecret, RedirectUrl, Scope, TokenResponse, TokenUrl,
-};
+use oauth2::{reqwest::async_http_client, AuthorizationCode, Scope, TokenResponse};
 use serde::Deserialize;
 use serde_json::{json, Value};
 
-use crate::config;
+use crate::{
+    app_state::AppState,
+    error::{Error, Result},
+};
 
 #[derive(Debug, Deserialize)]
 pub struct AuthRequest {
@@ -20,16 +18,8 @@ pub struct AuthRequest {
     state: String,
 }
 
-pub async fn oauth_redirect() -> impl IntoResponse {
-    // Initialize the OAuth2 client.
-    let client = BasicClient::new(
-        ClientId::new(config::GG_CLIENT_ID.to_string()),
-        Some(ClientSecret::new(config::GG_CLIENT_SECRET.to_string())),
-        AuthUrl::new(config::GG_AUTH_URL.to_string()).unwrap(),
-        Some(TokenUrl::new(config::GG_TOKEN_URL.to_string()).unwrap()),
-    )
-    .set_redirect_uri(RedirectUrl::new(config::GG_REDIRECT_URI.to_string()).unwrap());
-
+pub async fn oauth_redirect(State(state): State<AppState>) -> impl IntoResponse {
+    let client = state.gg_client;
     // Add the desired scopes
     let scopes = vec![
         Scope::new("https://www.googleapis.com/auth/userinfo.profile".to_string()),
@@ -46,22 +36,14 @@ pub async fn oauth_redirect() -> impl IntoResponse {
 
 pub async fn login_by_gg(
     Query(params): Query<AuthRequest>,
-) -> Result<Json<Value>, (StatusCode, String)> {
-    // Initialize the OAuth2 client.
-    let client = BasicClient::new(
-        ClientId::new(config::GG_CLIENT_ID.to_string()),
-        Some(ClientSecret::new(config::GG_CLIENT_SECRET.to_string())),
-        AuthUrl::new(config::GG_AUTH_URL.to_string()).unwrap(),
-        Some(TokenUrl::new(config::GG_TOKEN_URL.to_string()).unwrap()),
-    )
-    .set_redirect_uri(RedirectUrl::new(config::GG_REDIRECT_URI.to_string()).unwrap());
-
+    State(state): State<AppState>,
+) -> Result<Json<Value>> {
+    let client = state.gg_client;
     // Exchange the authorization code for an access token.
     let token_result = client
         .exchange_code(AuthorizationCode::new(params.code))
         .request_async(async_http_client)
         .await;
-
     // Handle the result of the token exchange.
     match token_result {
         Ok(token) => {
@@ -73,10 +55,7 @@ pub async fn login_by_gg(
         }
         Err(err) => {
             eprintln!("Error exchanging code for token: {:?}", err);
-            Err((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Failed to exchange authorization code for token: {}", err),
-            ))
+            Err(Error::from(err))
         }
     }
 }
